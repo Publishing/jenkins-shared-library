f90df98d2e998657a54f751fead66a8c1d15d730
abhirdwr80 <abhishek.tiwari@rte.ie>
2025-01-10 13:05:42 +0000

    Update lintCode.groovy

--- vars/lintCode.groovy
+++ vars/lintCode.groovy
@@ -1,5 +1,19 @@
 def call() {
-    sh "pip install ruff"
-    sh "ruff check ."
+    stage('Lint Code') {
+        steps {
+            script {
+                dir(env.RELEASE_NAME) {
+                    sh "pipenv run pip install ruff > ${TMP_DIR}/lint_install.log"
+                    env.LINT_OUT = sh(script: "pipenv run ruff check .", returnStdout: true).trim()
+                    if (!env.LINT_OUT.contains("All checks passed!")) {
+                        echo "Issue: ${env.LINT_OUT}"
+                        currentBuild.result = 'FAILURE'
+                        error("Stopping pipeline due to lint issues.")
+                    } else {
+                        echo "${env.LINT_OUT}"
+                    }
+                }
+            }
+        }
+    }
 }
-
