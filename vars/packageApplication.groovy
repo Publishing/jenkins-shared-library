b3f2a8e273ba7c807beb4802a8fd340e98636dae
abhirdwr80 <abhishek.tiwari@rte.ie>
2025-01-10 15:19:22 +0000

    Update packageApplication.groovy

--- vars/packageApplication.groovy
+++ vars/packageApplication.groovy
@@ -1,12 +1,25 @@
-def call() {
+def call(Map args) {
     stage('Package Application') {
         when {
-            expression { return params.SELECT_WORK_FLOW == 'CI-CD' }
+            expression { return params.SELECT_WORK_FLOW == 'CI-CD' } // Access pipeline parameter directly
         }
         steps {
             script {
-                sh "tar -cf ${env.RELEASE_NAME}.tar ${env.RELEASE_NAME}"
-                sh "cp -p ${env.RELEASE_NAME}.tar ${env.BACKUP_DIR}"
+                // Define variables from args
+                def releaseName = args.releaseName
+                def backupDir = args.backupDir
+
+                // Validate required parameters
+                if (!releaseName || !backupDir) {
+                    error "releaseName and backupDir are required but not provided in the configuration."
+                }
+
+                // Create tarball and copy to backup directory
+                echo "Packaging application..."
+                sh "tar -cf ${releaseName}.tar ${releaseName}"
+                sh "cp -p ${releaseName}.tar ${backupDir}"
+
+                echo "Application packaged successfully and stored in ${backupDir}"
             }
         }
     }
