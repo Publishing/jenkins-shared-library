6e6375bd4cca3e4ee243657a4a2bc2bb2154455b
abhirdwr80 <abhishek.tiwari@rte.ie>
2025-01-10 12:39:27 +0000

    Update runSonarQubeAnalysis.groovy

--- vars/runSonarQubeAnalysis.groovy
+++ vars/runSonarQubeAnalysis.groovy
@@ -1,1 +1,10 @@
+def call(Map config) {
+    sh """
+    sonar-scanner \
+    -Dsonar.projectKey=${config.projectKey} \
+    -Dsonar.sources=. \
+    -Dsonar.host.url=${config.sonarHost} \
+    -Dsonar.login=${config.sonarToken}
+    """
+}
 
