5df2549b309dd4be443145448590942f175ff8fc
abhirdwr80 <abhishek.tiwari@rte.ie>
2025-01-10 13:21:44 +0000

    Update packageApplication.groovy

--- vars/packageApplication.groovy
+++ vars/packageApplication.groovy
@@ -1,4 +1,13 @@
-def call(String appName) {
-    sh "tar -cf ${appName}.tar ."
+def call() {
+    stage('Package Application') {
+        when {
+            expression { return params.SELECT_WORK_FLOW == 'CI-CD' }
+        }
+        steps {
+            script {
+                sh "tar -cf ${env.RELEASE_NAME}.tar ${env.RELEASE_NAME}"
+                sh "cp -p ${env.RELEASE_NAME}.tar ${env.BACKUP_DIR}"
+            }
+        }
+    }
 }
-
