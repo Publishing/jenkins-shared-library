59eec63b9ace466844a224b0dfa0cc0251ee92d6
abhirdwr80 <abhishek.tiwari@rte.ie>
2025-01-10 15:17:37 +0000

    Update manualApproval.groovy

--- vars/manualApproval.groovy
+++ vars/manualApproval.groovy
@@ -1,19 +1,26 @@
-def call() {
+def call(Map args) {
     stage('Manual Approval') {
         when {
-            expression { return params.SELECT_WORK_FLOW == 'CI-CD' }
+            expression { return params.SELECT_WORK_FLOW == 'CI-CD' } // Access pipeline parameter directly
         }
         steps {
             script {
+                // Define optional variables from args
+                def inputMessage = args.inputMessage ?: "Proceed with deployment to server?"
+                def inputOkLabel = args.inputOkLabel ?: "Deploy"
+                def submitterParameter = args.submitterParameter ?: 'approver'
+
+                echo "Awaiting manual approval..."
+
                 // Capture the approver's Jenkins ID
-                def approval = input message: "Proceed with deployment to server?", 
-                                    ok: "Deploy",
-                                    submitterParameter: 'approver' // Store the approver's ID
+                def approval = input message: inputMessage, 
+                                    ok: inputOkLabel,
+                                    submitterParameter: submitterParameter
                 
                 // Retrieve the approver's email from Jenkins User API
                 def approverUser = Jenkins.instance.getUser(approval)
                 def approverEmail = approverUser?.getProperty(hudson.tasks.Mailer.UserProperty)?.getAddress()
-                
+
                 if (approverEmail) {
                     echo "Approver's Email: ${approverEmail}"
                     env.approver = approverEmail // Overwrite with the approver's email
