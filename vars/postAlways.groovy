2b180ebf72def074868caee17fe7d934af4ef6cf
abhirdwr80 <abhishek.tiwari@rte.ie>
2025-01-10 15:48:48 +0000

    Update and rename postBlock.groovy to postAlways.groovy

---
+++ vars/postAlways.groovy
@@ -0,0 +1,9 @@
+def call() {
+    try {
+        cleanWs() // Cleanup workspace
+        echo "Workspace cleanup completed."
+    } catch (Exception e) {
+        echo "Failed during postAlways: ${e.getMessage()}"
+        throw e
+    }
+}
