60ad5fc36e9a5e738308c41e28670e4ba498135b
abhirdwr80 <abhishek.tiwari@rte.ie>
2025-01-10 16:15:12 +0000

    using IF instead of WHEN

--- vars/packageApplication.groovy
+++ vars/packageApplication.groovy
@@ -1,10 +1,8 @@
 def call(Map args) {
     stage('Package Application') {
-        when {
-            expression { return params.SELECT_WORK_FLOW == 'CI-CD' } // Access pipeline parameter directly
-        }
-    
-            script {
+        script {
+            // Check if the workflow is CI-CD
+            if (params.SELECT_WORK_FLOW == 'CI-CD') {
                 // Define variables from args
                 def releaseName = args.releaseName
                 def backupDir = args.backupDir
@@ -20,7 +18,9 @@ def call(Map args) {
                 sh "cp -p ${releaseName}.tar ${backupDir}"
 
                 echo "Application packaged successfully and stored in ${backupDir}"
+            } else {
+                echo "Package Application stage skipped as workflow is not CI-CD."
             }
         }
     }
-
+}
