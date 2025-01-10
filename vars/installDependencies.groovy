08d5a8582e6313bc8e29a398241fef34e8e0371c
abhirdwr80 <abhishek.tiwari@rte.ie>
2025-01-10 15:12:39 +0000

    Update installDependencies.groovy

--- vars/installDependencies.groovy
+++ vars/installDependencies.groovy
@@ -1,13 +1,26 @@
-def call() {
+def call(Map args) {
     stage('Install Dependencies') {
         steps {
             script {
-                dir(env.RELEASE_NAME) {
+                // Define variables from args
+                def releaseName = args.releaseName
+                def pythonBinary = args.pythonBinary ?: '/usr/bin/python3' // Default to Python 3 if not provided
+                def verboseLogs = args.verboseLogs ?: false // Default to false if not provided
+
+                // Validate required parameters
+                if (!releaseName) {
+                    error "releaseName is required but not provided in the configuration."
+                }
+
+                // Navigate to the release directory
+                dir(releaseName) {
                     catchError(buildResult: 'FAILURE') {
-                        if (params.VERBOSE_LOGS) {
-                            sh "${env.PYTHON_BINARY} -m pipenv install --ignore-pipfile --verbose"
+                        if (verboseLogs) {
+                            echo "Installing dependencies with verbose logs..."
+                            sh "${pythonBinary} -m pipenv install --ignore-pipfile --verbose"
                         } else {
-                            sh "${env.PYTHON_BINARY} -m pipenv install --ignore-pipfile"
+                            echo "Installing dependencies..."
+                            sh "${pythonBinary} -m pipenv install --ignore-pipfile"
                         }
                     }
                 }
@@ -15,4 +28,3 @@ def call() {
         }
     }
 }
-
