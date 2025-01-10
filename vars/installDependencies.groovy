9c30659f9493a62dd70c3f08460fe5fb19db1cfc
abhirdwr80 <abhishek.tiwari@rte.ie>
2025-01-10 13:06:47 +0000

    Update installDependencies.groovy

--- vars/installDependencies.groovy
+++ vars/installDependencies.groovy
@@ -1,1 +1,18 @@
+def call() {
+    stage('Install Dependencies') {
+        steps {
+            script {
+                dir(env.RELEASE_NAME) {
+                    catchError(buildResult: 'FAILURE') {
+                        if (params.VERBOSE_LOGS) {
+                            sh "${env.PYTHON_BINARY} -m pipenv install --ignore-pipfile --verbose"
+                        } else {
+                            sh "${env.PYTHON_BINARY} -m pipenv install --ignore-pipfile"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
