9a4d8d943f2835db47788346a2b8aa8728ab0f0b
abhirdwr80 <abhishek.tiwari@rte.ie>
2025-01-10 16:59:55 +0000

    debugging tar file before packaging

--- vars/packageApplication.groovy
+++ vars/packageApplication.groovy
@@ -1,26 +1,35 @@
 def call(Map args) {
+    script {
+        // Check if the workflow is CI-CD
+        if (params.SELECT_WORK_FLOW == 'CI-CD') {
+            // Define variables from args
+            def releaseName = args.releaseName
+            def backupDir = args.backupDir
 
-        script {
-            // Check if the workflow is CI-CD
-            if (params.SELECT_WORK_FLOW == 'CI-CD') {
-                // Define variables from args
-                def releaseName = args.releaseName
-                def backupDir = args.backupDir
-
-                // Validate required parameters
-                if (!releaseName || !backupDir) {
-                    error "releaseName and backupDir are required but not provided in the configuration."
-                }
-
-                // Create tarball and copy to backup directory
-                echo "Packaging application..."
-                sh "tar -cf ${releaseName}.tar ${releaseName}"
-                sh "cp -p ${releaseName}.tar ${backupDir}"
-
-                echo "Application packaged successfully and stored in ${backupDir}"
-            } else {
-                echo "Package Application stage skipped as workflow is not CI-CD."
+            // Validate required parameters
+            if (!releaseName || !backupDir) {
+                error "releaseName and backupDir are required but not provided in the configuration."
             }
+
+            // Debug: Log current working directory and releaseName
+            echo "Current Working Directory: "
+            sh "pwd"
+            echo "Release Name: ${releaseName}"
+
+            // Debug: List contents of the release directory
+            echo "Contents of release directory (${releaseName}):"
+            sh "ls -l ${releaseName}"
+
+            // Create tarball and copy to backup directory
+            echo "Packaging application..."
+            sh "tar -cf ${releaseName}.tar ${releaseName}"
+            echo "Contents of backup directory (${backupDir}):"
+            sh "ls -l ${backupDir}"
+
+            sh "cp -p ${releaseName}.tar ${backupDir}"
+            echo "Application packaged successfully and stored in ${backupDir}"
+        } else {
+            echo "Package Application stage skipped as workflow is not CI-CD."
         }
     }
-
+}
