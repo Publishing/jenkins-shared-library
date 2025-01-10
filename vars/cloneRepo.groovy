09f5bf3b2071d700694cf5129300553e26d151b7
abhirdwr80 <abhishek.tiwari@rte.ie>
2025-01-10 12:38:38 +0000

    Update cloneRepo.groovy

--- vars/cloneRepo.groovy
+++ vars/cloneRepo.groovy
@@ -1,1 +1,10 @@
-clone repo
+def call(Map config) {
+    if (config.cloningOption == 'BRANCH') {
+        git branch: config.branch, url: config.repoUrl, credentialsId: config.credentialsId
+    } else if (config.cloningOption == 'TAG') {
+        git url: config.repoUrl, credentialsId: config.credentialsId
+        sh "git checkout tags/${config.tag}"
+    } else {
+        error "Invalid cloningOption: ${config.cloningOption}. Must be 'BRANCH' or 'TAG'."
+    }
+}
