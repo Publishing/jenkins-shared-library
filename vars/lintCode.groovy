354ba743bb52af485c110dc90f6ec97ab68f33d7
abhirdwr80 <abhishek.tiwari@rte.ie>
2025-01-22 12:19:01 +0000

    Urgent deployment logic

--- vars/lintCode.groovy
+++ vars/lintCode.groovy
@@ -1,5 +1,9 @@
 def call(Map args) {
        
+       if (params.SELECT_WORK_FLOW == 'UD') {
+        echo "Skipping Lint Code for workflow UD."
+        return
+            }
             script {
                 // Define variables from args
                 def releaseName = args.releaseName
