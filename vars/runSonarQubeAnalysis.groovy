9e6e9d2e8bb6b0d3df70a3df6c590bbf892d07bf
abhirdwr80 <abhishek.tiwari@rte.ie>
2025-02-21 16:58:43 +0000

    Update runSonarQubeAnalysis.groovy

--- vars/runSonarQubeAnalysis.groovy
+++ vars/runSonarQubeAnalysis.groovy
@@ -37,7 +37,7 @@ def call(Map args) {
                 // Keep track if we successfully got QG from Jenkins
                 boolean gotQGfromJenkins = false
                 env.SONAR_STATUS = ''  // Initialize
-                env.SONAR_URL = "${sonarHost}/api/qualitygates/project_status?projectKey=${appName}.app"
+                env.SONAR_URL = "${sonarHost}/dashboard?id=${appName}.app"
 
                 // 2) Attempt short waitForQualityGate (10 seconds)
                 try {
